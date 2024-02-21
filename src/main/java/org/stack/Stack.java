7efc74cc5af5137f5fbe79dcabb259f49dcf2a76
Aniome <vaganov.kostya2000@yandex.ru>
2024-02-22 03:05:49 +0500

    add stack class

---
+++ src/main/java/org/stack/Stack.java
@@ -0,0 +1,26 @@
+package org.stack;
+
+public class Stack {
+    private int[] array;
+    private int ind = 0;
+    public Stack(int length){
+        array = new int[length];
+    }
+
+    public int push(int data){
+        if (ind == array.length){
+            return -1;
+        }
+        array[ind] = data;
+        ind++;
+        return 0;
+    }
+
+    public int pop(){
+        if (ind == 0){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        ind--;
+        return array[ind+1];
+    }
+}
